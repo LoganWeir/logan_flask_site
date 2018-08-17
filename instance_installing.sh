6fa88744308076b4f2f6c080937648746eb47de9
Logan Weir <loganweir@gmail.com>
2018-08-17 15:44:41 -0700

    adding wsgi file

--- instance_installing.sh
+++ instance_installing.sh
@@ -8,3 +8,4 @@ sudo apt-get install nginx -y -q
 
 sudo apt-get install python3-pip python3-dev python3-venv build-essential libssl-dev libffi-dev python3-setuptools -y -q
 
+sudo git clone https://github.com/LoganWeir/logan_flask_site.git
