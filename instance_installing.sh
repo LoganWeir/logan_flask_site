9e8ce2b916e15ff850f7bd9e7977adf9c21c2006
Logan Weir <loganweir@gmail.com>
2018-08-17 15:40:23 -0700

    adding requirements

--- instance_installing.sh
+++ instance_installing.sh
@@ -6,3 +6,5 @@ sudo apt-get install git -y -q
 
 sudo apt-get install nginx -y -q
 
+sudo apt-get install python3-pip python3-dev python3-venv build-essential libssl-dev libffi-dev python3-setuptools -y -q
+
