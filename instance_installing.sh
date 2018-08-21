fea730f31ce500a4e3af065a4bffd2905354faec
Logan Weir <loganweir@gmail.com>
2018-08-20 20:59:02 -0700

    fixed instance shell script

--- instance_installing.sh
+++ instance_installing.sh
@@ -6,6 +6,10 @@ sudo apt-get install git -y -q
 
 sudo apt-get install nginx -y -q
 
-sudo apt-get install python3-pip python3-dev python3-venv build-essential libssl-dev libffi-dev python3-setuptools -y -q
+sudo apt-get install python3-pip python3-dev python3-venv -y -q
+
+sudo pip3 install virtualenv
 
 sudo git clone https://github.com/LoganWeir/logan_flask_site.git
+
+sudo mv /home/ubuntu/logan_flask_site/mysite.service /etc/systemd/system/mysite.service
