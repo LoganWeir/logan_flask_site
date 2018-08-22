7f7296cbb8b7ae3938287cb178e888a504ae701c
Logan Weir <loganweir@gmail.com>
2018-08-22 14:43:55 -0700

    removing sudo from service file

--- instance_installing.sh
+++ instance_installing.sh
@@ -8,8 +8,18 @@ sudo apt-get install nginx -y -q
 
 sudo apt-get install python3-pip python3-dev python3-venv -y -q
 
-sudo pip3 install virtualenv
-
 sudo git clone https://github.com/LoganWeir/logan_flask_site.git
 
 sudo mv /home/ubuntu/logan_flask_site/mysite.service /etc/systemd/system/mysite.service
+
+pwd
+
+sudo python3 -m venv /home/ubuntu/logan_flask_site/site/mysite_env/
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/logan_flask_site/site/mysite_env/
+
+source logan_flask_site/site/mysite_env/bin/activate
+
+pip install -r logan_flask_site/site/requirements.txt
+
+deactivate
